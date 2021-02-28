7eafd74f5706663c3133422b99e19cdd2f133c98
JOSEO <A311840@alumnos.uaslp.mx>
2021-02-28 11:21:21 -0600

    Implementar delete & get

--- src/uaslp/ingenieria/labs/Main.java
+++ src/uaslp/ingenieria/labs/Main.java
@@ -14,5 +14,44 @@ public class Main {
         lista1.add(4);
 
         System.out.println("El tamaño de la lista es:" + lista1.getSize());
+        System.out.println("----");
+
+        for(int i=0; i < lista1.getSize(); i++){
+            System.out.println("El dato de la posición " + i + " es:" + lista1.get(i));
+        }
+
+        System.out.println("-----------------------");
+
+        lista1.delete(1);
+
+        for(int i=0; i < lista1.getSize(); i++){
+            System.out.println("El dato de la posición " + i + " es:" + lista1.get(i));
+        }
+
+        System.out.println("-----------------------");
+
+        lista1.delete(0);
+
+        for(int i=0; i < lista1.getSize(); i++){
+            System.out.println("El dato de la posición " + i + " es:" + lista1.get(i));
+        }
+
+        System.out.println("-----------------------");
+
+        lista1.delete(lista1.getSize() - 1);
+
+        for(int i=0; i < lista1.getSize(); i++){
+            System.out.println("El dato de la posición " + i + " es:" + lista1.get(i));
+        }
+
+        System.out.println("-----------------------");
+
+        lista1.delete(0);
+
+        for(int i=0; i < lista1.getSize(); i++){
+            System.out.println("El dato de la posición " + i + " es:" + lista1.get(i));
+        }
+
+        System.out.println("El tamaño de la lista es:" + lista1.getSize());
     }
 }
--- src/uaslp/ingenieria/labs/list/LinkedList.java
+++ src/uaslp/ingenieria/labs/list/LinkedList.java
@@ -25,5 +25,55 @@ public class LinkedList {
         public int getSize() {
                 return size;
         }
+
+        public int get(int index){
+                Node currentNode = head;
+                int currentIndex = 0;
+
+                while(currentIndex < index){
+                        currentNode = currentNode.getNext();
+                        currentIndex++;
+                }
+
+                return currentNode.getData();
+        }
+
+        public void delete(int index){
+                Node currentNode = head;
+                int currentIndex = 0;
+
+                if(index < 0 || index >= size){
+                        return;
+                }
+
+                size--;
+
+                if(size == 0){
+                        head = null;
+                        tail = null;
+                        return;
+                }
+
+                if(index == 0){
+                        head = head.getNext();
+                        head.setPrevious(null);
+                }
+
+                if(index == size-1){
+                        tail = tail.getPrevious();
+                        tail.setNext(null);
+                }
+
+                if(index > 0 && index < size-1){
+
+                        while(currentIndex < index){
+                                currentNode = currentNode.getNext();
+                                currentIndex++;
+                        }
+
+                        currentNode.getPrevious().setNext(currentNode.getNext());
+                        currentNode.getNext().setPrevious(currentNode.getPrevious());
+                }
+        }
 }
 
